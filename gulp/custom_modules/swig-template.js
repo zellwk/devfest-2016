c9239fa65ee757bf840231ee1335ba41562d2511
Zell Liew <zellwk@gmail.com>
2015-07-31 13:11:46 +0800

    test changing to consolidate
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- gulp/custom_modules/swig-template.js
+++ gulp/custom_modules/swig-template.js
@@ -6,14 +6,18 @@ import path from 'path';
 import swig from 'swig';
 import swigMarked from 'swig-marked';
 import through from 'through2';
+import nunjucks from 'nunjucks';
+import consolidate from 'consolidate';
 
 // Get config from config.js
 import config from '../config';
 
+let engine = 'nunjucks';
+
 function swigTemplate(options) {
 
   let defaults = {
-    templateDir: 'src/templates',
+    templateDir: './src/templates',
     templateExt: '.swig',
     swigOptions: {},
     useTemplate: false
@@ -50,19 +54,27 @@ function swigTemplate(options) {
       localData = _.assign(localData, data);
     } catch (e) {}
 
+    // consolidates data
+    data = _.assign(globalData, frontmatterData, markdownData, localData);
+
     /**
-     * Gets templatePath for Swig
-     * @param  {string} options.template [template name]
-     * @return {string}                  Path to template 
-     * ----
+     * Figures out Template Dir for Nunjucks
      * Priority 1 : options given by user 
      * Priority 2 : template in frontmatter
      * Fallback   : Use self 
      */
-    
+
+    // if (options.template) {
+
+    // } else if (!_.isEmpty(frontmatterData) && frontmatterData.template) {
+
+    // } else {
+    //   console.log(path.dirname(file.path));
+    // }
+
     if (options.template) {
       templatePath = path.join(process.cwd(), options.templateDir, options.template + options.templateExt);
-      
+
       try {
         fs.openSync(templatePath, 'r');
       } catch (e) {
@@ -77,32 +89,40 @@ function swigTemplate(options) {
       } catch (e) {
         cb(pluginError(`${frontmatterData.template}${options.templateExt} not found in ${options.templateDir}`))
       }
-      
+
     } else {
       // use self if no templates found 
-      templatePath = file.path;
+      console.log(path.resolve(__dirname));
+      // console.log(path.dirname(file.path));
+      templatePath = './src/templates/testing.nunjucks';
+      // templatePath = file.path;
     }
 
     // Sets swig cache to false to
-    swigOptions = _.assign(options.swigOptions, {cache: false});
+    // swigOptions = _.assign(options.swigOptions, {cache: false});
 
-    // Adds Marked tag & filter to Swig
-    swigMarked.useTag(swig);
-    swigMarked.useFilter(swig);
-    swigMarked.configure(config.blog.markdownOptions);
+    // // Adds Marked tag & filter to Swig
+    // swigMarked.useTag(swig);
+    // swigMarked.useFilter(swig);
+    // swigMarked.configure(config.blog.markdownOptions);
 
-    // Compiles Template 
-    swig.setDefaults(options.swigOptions);
-    try {
-      template = swig.compileFile(templatePath);
-    } catch (e) {
-      console.log(e);
-    }
+    // // Compiles Template 
+    // swig.setDefaults(options.swigOptions);
+    // try {
+    //   template = swig.compileFile(templatePath);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    // 
+    // template = template.render(data);
+    // console.log(template);
+    // file.contents = new Buffer(template(data));
 
-    // Renders template with data
-    data = _.assign(globalData, frontmatterData, markdownData, localData);
-
-    file.contents = new Buffer(template(data));
+    // TemplateFolder
+    // Render Template.ext 
+    consolidate[engine](templatePath, data, (err, html)=> {
+      file.contents = new Buffer(html);
+    });
 
     cb(null, file);
   });
