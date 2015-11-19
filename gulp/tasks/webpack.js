8908f4eb2f409728a42f375aa9bc0c5fff29e201
Zell Liew <zellwk@gmail.com>
2015-11-19 17:08:56 +0800

    reconfigure webpack
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -1,7 +1,9 @@
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import webpack from 'webpack';
 import webpackStream from 'webpack-stream';
 import browserSync from 'browser-sync';
+import notifier from 'node-notifier'; 
 
 // Import configs
 import config from '../config';
@@ -15,8 +17,34 @@ let $ = plugins();
 // TODO: Setup React Hot Loader with Browsersync + webpack
 gulp.task('webpack', function() {
   return gulp.src(config.webpack.src)
-    .pipe(webpackStream(config.webpack.options, webpack))
-    .pipe(gulp.dest(config.webpack.dest))
+    .pipe(plumber('Error Running Webpack'))
+    .pipe(webpackStream(config.webpack.options, webpack, (err, stats) => {
+      if (stats.compilation.errors.length) {
+        // Notifiers if errors
+        notifier.notify({
+          title: 'Webpack error',
+          message: stats.compilation.errors[0].error,
+          sound: 'Frog',
+        });
+
+        // Logs Errors
+        var errMsg = stats.compilation.errors[0];
+        // delete errMsg['message'];
+        
+        // Removes extra logs (because I was unable to get error message only)
+        delete errMsg['name'];
+        delete errMsg['dependencies'];
+        delete errMsg['module'];
+        delete errMsg['origin'];
+        delete errMsg['DependenciesBlock'];
+        delete errMsg['error'];
+
+        // Logs Errors
+        gutil.log(errMsg);
+      }
+    }))
+
+  .pipe(gulp.dest(config.webpack.dest))
     .pipe(browserSync.reload({
       stream: true
     }));
