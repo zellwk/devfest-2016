df6375fee5dffc1274df7f9eca601450bdc2c622
Zell Liew <zellwk@gmail.com>
2016-02-14 10:48:29 +0800

    removed unneeded files
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- _site.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Pagination and permalinks 
-  // relative_permalinks: false,
-  // permalink: '/blog/:title/',
-  // paginate: '8',
-  // paginate_path: "/blog/page:num",
-};
