48e784d238d09fc835e9dd18708f19b87e51351b
Zell Liew <zellwk@gmail.com>
2016-04-26 00:44:19 +0800

    update webpack conf
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,28 +1,32 @@
-import environment from './env';
-import BowerWebpackPlugin from 'bower-webpack-plugin';
-
-let env = environment.env;
+const environment = require('./env')
+const BowerWebpackPlugin = require('bower-webpack-plugin')
+const env = environment.env
 
 module.exports = {
   options: {
-    watch: env === 'prod' ? false : true,
+    watch: env !== 'prod',
     output: {
       filename: '[name].js',
       pathinfo: true
     },
     resolve: {
-      root: './src/bower_components/',
+      root: [
+        './src/bower_components/'
+      ]
       // Using aliases (if needed)
       // alias: { 'TweenLite': 'gsap/src/uncompressed/TweenLite' }
     },
-    devtool: 'source-map',
+    devtool: '#source-map',
     module: {
       loaders: [{
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
+      }, {
+        test: /\.(nunj|nunjucks)$/,
+        loader: 'nunjucks-loader'
       }],
       plugins: [new BowerWebpackPlugin()]
     }
   }
-};
+}
