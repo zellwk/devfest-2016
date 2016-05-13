6a1172bd7e5985c11daf72516d487d677df4b1b1
Zell Liew <zellwk@gmail.com>
2016-05-14 06:07:18 +0800

    modified deploy.sh
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
-# http://www.ryanburnette.com/blog/2015/deploy-to-another-branch.html
-# git add dist -f
-# git commit -m "deploy"
-# git push origin `git subtree split --prefix dist master`:production --force
-# git reset HEAD~1 --hard
-
+set -e
+git stash save
+gulp --prod
 REV=`git rev-parse HEAD`
-git checkout ghpages
-find . -and -not -path "./.git*" -and -not -path "./node_modules*" -and -not -path "./src*" -and -not -path "./dev*" -and -not -path "./dist*" |  xargs git rm -rf --ignore-unmatch
+git checkout gh-pages
+git rm -rf .
+git checkout master -- .gitignore
 mv dist/* . && rm -rf dist # might to be changed if you have hidden files
 git add .
-git add -u
 git commit -m "deployed $REV"
 git push --all
 git checkout master
+git stash pop
