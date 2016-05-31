5f5c88e72c9b5b05397d63082615331fe13a6fd1
Zell Liew <zellwk@gmail.com>
2016-05-31 18:52:33 +0800

    fix form functions
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,70 @@
 /* globals $ */
 import svg4everybody from 'svg4everybody'
-var $ = require('jquery')
+let $ = require('jquery')
 require('./jqueryform')
 
 // Polyfill for external SVG spritesheets
 svg4everybody()
 
 // Form submission
-
 $(document).ready(function () {
-  var $form = $('#subscribeForm')
-  var $text = $('#subscribeForm .msg').find('span')
-  var $spinner = $form.find('.spinner')
-  var $social = $('.social')
-
   $('.subscribeForm').ajaxForm({
     url: 'http://2014.cssconf.asia/addsubscriber.php',
     dataType: 'html',
-    beforeSubmit: function () {
+    beforeSubmit: function (fields, $form) {
+      let $spinner = $form.find('.spinner')
+      let $msg = $form.siblings('.msg')
+      let $text = $msg.find('span')
+      let $btn = $form.find('.btn')
+      let $padding = $btn.width() - $text.width()
+
       // Resets text before submitting
-      $text.text('')
-      // Starts spinner
+      let $btnHeight = $btn.outerHeight()
+      $btn.css('height', $btnHeight)
+      $btn.animate({
+        width: $padding
+      }, 400, 'swing')
+      $btn.find('.button-text').text('')
       $spinner.show().addClass('play')
-      $social.hide()
     },
-    success: function (r) {
+    success: function (r, status, response, context) {
+      let $form = $(context[0])
+      let $spinner = $form.find('.spinner')
+      let $msg = $form.siblings('.msg')
+      let $text = $msg.find('span')
+
       if (r.substr(0, 6) !== 'Thanks') {
         console.log('No Thanks')
         $text.text(r.substr(0, r.indexOf('<br/>')))
       } else {
         console.log('Thanks')
-        $form.find('input').val('')
-        $text.text('Thanks. We\'ll keep you updated!')
-
-        $form.find('.social-msg').text('Check out our facebook page now! :)')
-        // $social.show().addClass('form-show');
+        $spinner.removeClass('play')
+        $form.siblings('p').fadeOut(300)
+        $form.fadeOut('300', () => {
+          $text.text('Thanks. We\'ll keep you updated!')
+          $msg.fadeIn('300')
+        })
 
         setTimeout(function () {
-          window.location.href = 'http://facebook.com/devfestasia'
+          // window.location.href = 'http://facebook.com/devfestasia'
         }, 2000)
       }
     },
-    error: function (r, s) {
-      // $("#subscribeForm").removeClass("load").addClass("failure");
-      $text.text('Something went utterly wrong...')
+    error: function (r, status, response, context) {
+      let $form = $(context[0])
+      let $spinner = $form.find('.spinner')
+      let $msg = $form.siblings('.msg')
+      let $text = $msg.find('span')
+
+      $spinner.removeClass('play')
+      $form.siblings('p').fadeOut(300)
+      $form.fadeOut('300', () => {
+        $text.text('Something went utterly wrong...')
+        $msg.fadeIn('300')
+      })
     },
     complete: function () {
       console.log('complete')
-      $spinner.hide().removeClass('play')
-      // Stops spinner
     }
   })
 })
