f89430c6bf2ecb4a27ae40879db92f7c21bedffe
Zell Liew <zellwk@gmail.com>
2016-07-10 15:09:21 +0800

    add show full text
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,34 @@ svg4everybody()
 $(window).resize(function (event) {
   console.log($(window).width())
 })
+
+if ($('.jsShowMore').length) {
+  let $showMore = $('.jsShowMore')
+  $showMore.each((index, el) => {
+    let $el = $(el)
+    let fullText = $el.html()
+    let shortenedText
+    let maxLength = 400
+    $el.attr('full-text', fullText)
+
+    if (fullText.length > 400) {
+      shortenedText = fullText.substr(0, maxLength)
+      shortenedText = shortenedText.substr(0, Math.min(shortenedText.length, shortenedText.lastIndexOf(' ')))
+      shortenedText += '... <a href="#" class="jsSeeMoreLink"> See More </a>'
+      $el.html(shortenedText)
+    }
+  })
+
+  function showFullText (e) {
+    e.preventDefault()
+    let $target = $(e.target)
+    let $container = $(e.target).closest('.jsShowMore')
+    let fullText = $container.attr('full-text')
+    $container.html(fullText)
+    $target.off('click', showFullText)
+  }
+
+  $('.jsSeeMoreLink').on('click', showFullText)
+
+}
+
