8ffdffd266f1a9deaf834ad8dbab5e1d07687482
Zell Liew <zellwk@gmail.com>
2016-09-29 11:56:20 +0800

    fix js bug
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- src/js/jobsroll.js
+++ src/js/jobsroll.js
@@ -2,19 +2,21 @@ const tpl = require('../templates/partials/events/jobs-placement.nunjucks')
 const data = require('../../data/jobs.json')
 const shuffle = require('shuffle-array')
 
-let companies = data.jobs
-let jobs = companies.reduce((initial, curr) => {
-  let newMap = curr.jobs.map(el => {
-    el.employer = curr.employer
-    el.image = curr.image
-    return el
-  })
-  return initial.concat(newMap)
-}, [])
+const jobPlacement = document.querySelector('.jsJobsPlacement')
+if (jobPlacement) {
+  let companies = data.jobs
+  let jobs = companies.reduce((initial, curr) => {
+    let newMap = curr.jobs.map(el => {
+      el.employer = curr.employer
+      el.image = curr.image
+      return el
+    })
+    return initial.concat(newMap)
+  }, [])
 
-const html = tpl.render({
+  const html = tpl.render({
   jobs: shuffle(jobs)
 })
 
-const jobPlacement = document.querySelector('.jsJobsPlacement')
-jobPlacement.innerHTML = html
+  jobPlacement.innerHTML = html
+}
